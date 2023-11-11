6df97d9d12d9aa17e4f6ae93a5778af5b0ba83b2
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-11 02:15:28 -0300

    add: GET/teams and POST/teams/reset

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ app.use(express.text('*/*'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/', teamsRouter)
+app.use('/api', teamsRouter)
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`App listening on port ${PORT}`)
 })
--- src/routes/teams.js
+++ src/routes/teams.js
@@ -1,8 +1,24 @@
 const Router = require('express').Router
 const teamRouter = new Router()
+const { getTeams, resetTeams } = require('../api/teams.js')
 
-teamRouter.get('/teams', (req, res) => {
-  res.send('GET /teams')
+teamRouter.get('/teams', (_, res) => {
+  try {
+    const teams = getTeams()
+    res.send(teams)
+  } catch (error) {
+    res.send(500).send(error)
+  }
+})
+
+teamRouter.post('/teams/reset', (_, res) => {
+  try {
+    resetTeams()
+  } catch (error) {
+    res.send(400).send(error)
+  }
+
+  res.status(200).send('Teams resetted successfully')
 })
 
 module.exports = teamRouter

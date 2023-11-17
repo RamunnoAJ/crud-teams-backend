4cd054cbf37cddce32179a1e5c78fdab9b992ad1
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-16 22:25:10 -0300

    add: cors middleware

--- src/app.js
+++ src/app.js
@@ -9,7 +9,14 @@ app.use(express.json())
 app.use(express.text('*/*'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 app.use('/api', teamsRouter)
 
 app.listen(PORT, () => {

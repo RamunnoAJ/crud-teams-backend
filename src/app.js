c26d136412774bf779aa048d4e58a518819cd717
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-19 18:29:59 -0300

    fix: for god's sake you have to work now

--- src/app.js
+++ src/app.js
@@ -27,6 +27,14 @@ app.use(express.json())
 app.use(express.text('*/*'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 
 app.use('/api', teamsRouter)
 

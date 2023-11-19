e14fb59fbd31a02e4b19a696fa3422e2240d572a
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-19 18:01:46 -0300

    fix: cors

--- src/app.js
+++ src/app.js
@@ -11,16 +11,9 @@ app.use(express.json())
 app.use(express.text('*/*'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  )
-  next()
-})
+
 app.use('/api', teamsRouter)
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
 })

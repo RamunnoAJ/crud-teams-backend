6445e558e0605cc0157c7183b4f74995988f9d28
RamunnoAJ <ramunnoagustin@gmail.com>
2023-11-19 18:21:29 -0300

    fix: cors problems

--- src/app.js
+++ src/app.js
@@ -6,7 +6,23 @@ const teamsRouter = require('./routes/teams.js')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'x-client-key',
+      'x-client-token',
+      'x-client-secret',
+      'Authorization',
+    ],
+  }),
+)
 app.use(express.json())
 app.use(express.text('*/*'))
 app.use(express.static('public'))
